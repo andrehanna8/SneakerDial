b1c7b8ace2856d07d5ed01311d445d2687283bba
andrehanna8 <andrehanna3@gmail.com>
2023-02-03 21:47:46 -0800

    update

--- dist/wheelscript.js
+++ dist/wheelscript.js
@@ -30,3 +30,10 @@ function toggleClassName(inElement, inClassName)
 }
 
 
+  let root = document.documentElement;
+
+root.addEventListener("mousemove", e => {
+  root.style.setProperty('--mouse-x', e.clientX + "px");
+  root.style.setProperty('--mouse-y', e.clientY + "px");
+  console.log(e.clientX)
+});
